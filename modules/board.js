c2d7cb5621ded93ba2f725036537af798eb26d72
Angry-Potato <liam.humphreys1989@gmail.com>
2015-07-26 10:51:23 +0100

    Add height and width and default values to board

--- modules/board.js
+++ modules/board.js
@@ -1,5 +1,12 @@
-function Board() {
-	this.places = {};
+/*
+ * Board class
+ */
+function Board(places, height, width) {
+	// initialise empty board
+	this.height = height ? height : 3;
+	this.width = width ? width : 3;
+	this.places = places ? places : [];
 }
+
 // export the class
 module.exports = Board;
--- test/board.js
+++ test/board.js
@@ -1,13 +1,19 @@
 var Board = require("../modules/board.js");
 
 suite("Board", function() {
-	suite("Initial state", function() {
+	suite("Initial default state", function() {
 		var subject = null;
 		setup(function() {
 			subject = new Board();
 		});
 		test("should have places", function() {
-			subject.should.have.property('places');
+			subject.should.have.property('places', []);
+		});
+		test("should have height", function() {
+			subject.should.have.property('height', 3);
+		});
+		test("should have width", function() {
+			subject.should.have.property('width', 3);
 		});
 	});
 });
