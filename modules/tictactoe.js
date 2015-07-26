46ba987cd203be07472628bf1e128609216e1681
Angry-Potato <liam.humphreys1989@gmail.com>
2015-07-26 12:00:29 +0100

    Create member variables in TicTacToe game class

--- modules/tictactoe.js
+++ modules/tictactoe.js
@@ -1,7 +1,13 @@
+var Board = require('./board.js');
+var Player = require('./player.js');
+
 /*
  * Tictactoe class
+ * Runs a game of tictactoe between two machine players
  */
 function TicTacToe() {
+	this.players = [new Player("x"), new Player("o")];
+	this.board = new Board(3, 3);
 }
 
 // export the class
