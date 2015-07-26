cf4b8e52db6407b7648100292848f261b2a175d9
Angry-Potato <liam.humphreys1989@gmail.com>
2015-07-26 11:35:29 +0100

    Re-order board constructor arguments, implement creation of empty board on construction

--- modules/board.js
+++ modules/board.js
@@ -1,12 +1,26 @@
+var Place = require('./place.js');
+
 /*
  * Board class
+ * Maintains an array representing all places on a game board.
  */
-function Board(places, height, width) {
+function Board(width, height) {
 	// initialise empty board
-	this.height = height ? height : 3;
 	this.width = width ? width : 3;
-	this.places = places ? places : [];
+	this.height = height ? height : 3;
+	this.places = this.getEmptyBoard();
 }
+
+//gets an array of empty pieces according to the height and width of the board
+Board.prototype.getEmptyBoard = function() {
+	var places = [];
+	for (var y = 0; y < this.height; y++) {
+		for (var x = 0; x < this.width; x++) {
+			places.push(new Place(x, y, " "));
+		}
+	}
+	return places;
+};
 
 // export the class
 module.exports = Board;
--- test/board.js
+++ test/board.js
@@ -6,14 +6,29 @@ suite("Board", function() {
 		setup(function() {
 			subject = new Board();
 		});
-		test("should have places", function() {
-			subject.should.have.property('places', []);
+		test("should have default width", function() {
+			subject.should.have.property('width', 3);
 		});
-		test("should have height", function() {
+		test("should have default height", function() {
 			subject.should.have.property('height', 3);
 		});
-		test("should have width", function() {
-			subject.should.have.property('width', 3);
+		test("should have default number of places", function() {
+			subject.should.have.property('places').with.lengthOf(9);
+		});
+	});
+	suite("After construction", function() {
+		var subject = null;
+		setup(function() {
+			subject = new Board(23, 42);
+		});
+		test("should have the set width", function() {
+			subject.should.have.property('width', 23);
+		});
+		test("should have the set height", function() {
+			subject.should.have.property('height', 42);
+		});
+		test("should have the correct number of places according to height and width", function() {
+			subject.should.have.property('places').with.lengthOf(23*42);
 		});
 	});
 });
