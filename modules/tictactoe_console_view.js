cbf8d427ab662272e8bb5b4fd62e10e78b84ec3e
Angry-Potato <liam.humphreys1989@gmail.com>
2015-07-27 01:52:10 +0100

    Tictactoe console view prints welcome message

--- modules/tictactoe_console_view.js
+++ modules/tictactoe_console_view.js
@@ -7,7 +7,10 @@ function TicTacToeConsoleView(outputConsole) {
 }
 
 TicTacToeConsoleView.prototype.welcome = function() {
-
+	this.console.log("Welcome to a vicious match of Tic-Tac-Toe to the death!");
+	this.console.log("In the eccentric \"x\" corner, we have Player 1");
+	this.console.log("In the well-rounded \"o\" corner, we have Player 2");
+	this.console.log("Behold their capacious arena, a strategical minefield in 3x3 form:");
 };
 
 // export the class
