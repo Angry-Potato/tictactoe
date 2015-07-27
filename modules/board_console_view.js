23eae0d87b398c9a45c8f6b0f861b3c5394469b0
Angry-Potato <liam.humphreys1989@gmail.com>
2015-07-27 01:27:33 +0100

    Refactor board console view and improve test coverage

--- modules/board_console_view.js
+++ modules/board_console_view.js
@@ -2,32 +2,65 @@
  * Board Console View class
  * Given an array of places, will render them in grid fashion to the console
  */
-function BoardConsoleView() {
-	
+function BoardConsoleView(outputConsole) {
+	this.horizontalPlacePadding = 1;
+	this.verticalPlacePadding = 1;
+	this.pad = " ";
+	this.verticalLine = "|";
+	this.horizontalLine = "_";
+	this.width = 0;
+	this.height = 0;
+	this.console = outputConsole ? outputConsole : console;
 }
 
 BoardConsoleView.prototype.render = function(places) {
+	this.calculateDimensions(places.length);
 	var placeCount = 0;
-	for (var y = 0; y < 9; y++) {
+	for (var y = 0; y < this.height; y++) {
 		var row = "";
-		for (var x = 0; x < 11; x++) {
-			if (x !== 0 && (x + 1) % 4 === 0) {
-				row += "|";
+		for (var x = 0; x < this.width; x++) {
+			if (this.isColumnVerticalLine(x)) {
+				row += this.verticalLine;
 			}
-			else if (y !== 8 && (y + 1) % 3 === 0) {
-				row += "_";
+			else if (this.isRowHorizontalLine(y)) {
+				row += this.horizontalLine;
 			}
-			else if ((x - 1) % 4 === 0 && (y - 1) % 3 === 0) {
+			else if (this.isCenterOfBox(x,y)) {
 				row += places[placeCount++].getValue();
 			}
 			else {
-				row += " ";
+				row += this.pad;
 			}
 		}
-		console.log(row);
+		this.console.log(row);
 	}
 };
 
+BoardConsoleView.prototype.calculateDimensions = function(placeCount) {
+	var sqRoot = Math.sqrt(placeCount);
+	this.width = this.calculateWidth(placeCount, sqRoot);
+	this.height = this.calculateHeight(placeCount, sqRoot);
+};
+
+BoardConsoleView.prototype.calculateWidth = function(placeCount, sqRoot) {
+	return (this.horizontalPlacePadding*2 + 1)*sqRoot + (sqRoot-1);
+};
+
+BoardConsoleView.prototype.calculateHeight = function(placeCount, sqRoot) {
+	return (this.verticalPlacePadding*2 + 1)*sqRoot;
+};
+
+BoardConsoleView.prototype.isColumnVerticalLine = function(x) {
+	return x > 0 && (x + 1) % (this.horizontalPlacePadding*2 + 2) === 0;
+};
+
+BoardConsoleView.prototype.isRowHorizontalLine = function(y) {
+	return y < this.height-1 && (y + 1) % (this.verticalPlacePadding*2 + 1) === 0;
+};
+
+BoardConsoleView.prototype.isCenterOfBox = function(x,y) {
+	return (x - 1) % (this.horizontalPlacePadding*2 + 2) === 0 && (y - 1) % (this.verticalPlacePadding*2 + 1) === 0;
+};
 
 // export the class
 module.exports = BoardConsoleView;
--- test/board_console_view.js
+++ test/board_console_view.js
@@ -1,12 +1,106 @@
-var BoardConsoleView = require("../modules/board_console_view.js");
+var BoardConsoleView = require("../modules/board_console_view.js"),
+	Place = require("../modules/place.js"),
+	sinon = require('sinon');
 
 suite("BoardConsoleView: ", function() {
 
 	suite("Initial default state", function() {
 		var subject = null;
-		
+
 		setup(function() {
 			subject = new BoardConsoleView();
+		});
+
+		test("should have a horizontalPlacePadding", function() {
+			subject.should.have.property('horizontalPlacePadding');
+		});
+		
+		test("should have a verticalPlacePadding", function() {
+			subject.should.have.property('verticalPlacePadding');
+		});
+		
+		test("should have a pad", function() {
+			subject.should.have.property('pad');
+		});
+		
+		test("should have a verticalLine", function() {
+			subject.should.have.property('verticalLine');
+		});
+		
+		test("should have a horizontalLine", function() {
+			subject.should.have.property('horizontalLine');
+		});
+		
+		test("should have a width", function() {
+			subject.should.have.property('width');
+		});
+		
+		test("should have a height", function() {
+			subject.should.have.property('height');
+		});
+		
+		test("should have a console", function() {
+			subject.should.have.property('console');
+		});
+	});
+	suite("After construction", function() {
+		var subject = null;
+		var consoleLogStub = null;
+
+		setup(function() {
+			subject = new BoardConsoleView();
+			consoleLogStub = sinon.stub(subject.console, 'log');
+		});
+
+		teardown(function() {
+			subject.console.log.restore();
+		});
+
+		test("should render to the console", function() {
+			var places = [new Place(1,2," "),new Place(1,2," "),new Place(1,2," "),new Place(1,2," ")];
+			subject.render(places);
+
+			consoleLogStub.callCount.should.not.be.below(places.length);
+		});
+
+		test("should render a grid", function() {
+			var places = [];
+			for (var y = 0; y < 3; y++) {
+				for (var x = 0; x < 3; x++) {
+					places.push(new Place(x, y, " "));
+				}
+			}
+			subject.render(places);
+
+			consoleLogStub.getCall(0).args[0].should.eql("   |   |   ");
+			consoleLogStub.getCall(1).args[0].should.eql("   |   |   ");
+			consoleLogStub.getCall(2).args[0].should.eql("___|___|___");
+			consoleLogStub.getCall(3).args[0].should.eql("   |   |   ");
+			consoleLogStub.getCall(4).args[0].should.eql("   |   |   ");
+			consoleLogStub.getCall(5).args[0].should.eql("___|___|___");
+			consoleLogStub.getCall(6).args[0].should.eql("   |   |   ");
+			consoleLogStub.getCall(7).args[0].should.eql("   |   |   ");
+			consoleLogStub.getCall(8).args[0].should.eql("   |   |   ");
+		});
+
+		test("should render a grid with pieces", function() {
+			var places = [];
+			for (var y = 0; y < 3; y++) {
+				for (var x = 0; x < 3; x++) {
+					places.push(new Place(x, y, "x"));
+				}
+			}
+			subject.render(places);
+
+			consoleLogStub.getCall(0).args[0].should.eql("   |   |   ");
+			consoleLogStub.getCall(1).args[0].should.eql(" x | x | x ");
+			consoleLogStub.getCall(2).args[0].should.eql("___|___|___");
+			consoleLogStub.getCall(3).args[0].should.eql("   |   |   ");
+			consoleLogStub.getCall(4).args[0].should.eql(" x | x | x ");
+			consoleLogStub.getCall(5).args[0].should.eql("___|___|___");
+			consoleLogStub.getCall(6).args[0].should.eql("   |   |   ");
+			consoleLogStub.getCall(7).args[0].should.eql(" x | x | x ");
+			consoleLogStub.getCall(8).args[0].should.eql("   |   |   ");
 		});
 	});
 });
